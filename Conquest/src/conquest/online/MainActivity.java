db9228e83213edd42cc59c2b237533b151bd2438
oleksiygubin <ogubin@iastate.edu>
2014-10-06 22:11:40 -0500

    made a method to check if device can connect to google play services

--- Conquest/src/conquest/online/MainActivity.java
+++ Conquest/src/conquest/online/MainActivity.java
@@ -1,17 +1,26 @@
 package conquest.online;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
 	//Key to define for activities to access previous activity info
 	public final static String EXTRA_MESSAGE = "conquest.online.MESSAGE";
+	private static final int GPS_ERRORDIALOG_REQUEST = 9001;
+	
+	
 	
     @Override
     //This defines your first page to be started.
@@ -39,6 +48,24 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    //method used to check if device is connected to google play services
+    public boolean servicesOK()
+    {
+    	int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+    	
+    	if(isAvailable == ConnectionResult.SUCCESS){
+    		return true;
+    	}
+    	else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailable)){
+    		Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, GPS_ERRORDIALOG_REQUEST);
+    		dialog.show();
+    	}
+    	else{
+    		Toast.makeText(this, "Can't Connect to Google Play Services", Toast.LENGTH_SHORT).show();
+    	}
+    	return false;
     }
     
     /** Called when the user clicks the send button on the main activity - THIS IS FOR TESTING*/
