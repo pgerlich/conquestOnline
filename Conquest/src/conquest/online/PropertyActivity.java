b854bf0387459da775130c4efd409ab357615a6e
oleksiygubin <ogubin@iastate.edu>
2014-11-06 18:29:46 -0600

    this is all my commits together at once so my codes on hur :D

--- Conquest/src/conquest/online/PropertyActivity.java
+++ Conquest/src/conquest/online/PropertyActivity.java
@@ -1,9 +1,12 @@
 package conquest.online;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class PropertyActivity extends Activity {
 
@@ -22,5 +25,11 @@ public class PropertyActivity extends Activity {
 	public void loadProperty() {
 		//parameters coordinates maybe
 		//load images and such
+	}
+	public void goToNewProp(View view){
+		finish();
+		//Toast.makeText(this, "ASSNTITS", Toast.LENGTH_SHORT).show();
+		Intent newproperty = new Intent(this, NewPropertyActivity.class);
+		startActivity(newproperty);
 	}
 }
