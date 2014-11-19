ea085745c1337168007e9d3aa36dd64f19e14d44
Paul <pgerlich@iastate.edu>
2014-11-18 22:00:52 -0600

    Added new settings screen with required files. Working on social screen

--- Conquest/src/conquest/online/NewSocialActivity.java
+++ Conquest/src/conquest/online/NewSocialActivity.java
@@ -1,14 +1,27 @@
 package conquest.online;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import conquest.online.SocialActivity.RetrievePeople;
+import conquest.online.SocialActivity.addPlayer;
 
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
@@ -17,7 +30,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewSocialActivity extends Activity {
 
@@ -34,12 +51,23 @@ public class NewSocialActivity extends Activity {
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	
+	private UserSession user;
+	public ArrayList<String> friends;
+	public ArrayList<String> guilds;
+	public ArrayList<String> enemies;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_social);
-
+		user = new UserSession(getApplicationContext());
+		
+		//Create user session and friends list
+		friends = new ArrayList<String>(10);
+		guilds = new ArrayList<String>(10);
+		enemies = new ArrayList<String>(10);
+		
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the activity.
 		mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
@@ -47,6 +75,18 @@ public class NewSocialActivity extends Activity {
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+		
+		//Retrieve friends
+		RetrievePeople grabFriends = new RetrievePeople(user.getUser(), user.getToken(), "friends");
+		grabFriends.execute();
+		
+		//Retrieve guild members
+		RetrievePeople grabGuild = new RetrievePeople(user.getUser(), user.getToken(), "guild");
+		grabGuild.execute();
+		
+		//Retrieve enemies
+		RetrievePeople grabEnemies = new RetrievePeople(user.getUser(), user.getToken(), "enemies");
+		grabEnemies.execute();
 
 	}
 
@@ -56,6 +96,55 @@ public class NewSocialActivity extends Activity {
 		getMenuInflater().inflate(R.menu.new_social, menu);
 		return true;
 	}
+	
+	public void toast(String message) {
+		//Toast an error message if it exists. Will close and leave page if it doesn't
+		Context context = getApplicationContext();
+		CharSequence text = message;
+		int duration = Toast.LENGTH_SHORT;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	}
+	
+	/**
+	 * called when the user hits the add friend button
+	 */
+	public void addFriend(View view) {
+		EditText usr = (EditText) findViewById(R.id.newFriend);
+		String name = usr.getText().toString();
+		
+		
+		addPlayer add = new addPlayer(user.getUser(), name, user.getToken());
+		add.execute();
+	}
+	
+	/** 
+	 * Display friends by iterating through the arraylist of friends
+	 */
+	public void listPeople(String type) {
+		ArrayList<String> persons = null;
+		LinearLayout showPersons = (LinearLayout) findViewById(R.id.socialDisplay);
+		
+		//So this is a generic method called for friends, enemies, and guild members. This just specifies it
+		if ( type.equals("friends") ) {
+			persons = friends;
+		} else if ( type.equals("guild") ) {
+			persons = guilds;
+		} else if ( type.equals("enemies") ) {
+			persons = enemies;
+		}
+		
+		((LinearLayout) showPersons).removeAllViews();
+				
+		for (String person : persons) {
+			TextView f = new TextView(this);
+			f.setText(person);
+			f.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.WRAP_CONTENT));
+			((LinearLayout) showPersons).addView(f);
+		}
+	}
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -172,4 +261,169 @@ public class NewSocialActivity extends Activity {
 		}
 	}
 
+	/**
+	 * Represents an asynchronous login/registration task used to authenticate
+	 * the user.
+	 */
+	public class RetrievePeople extends AsyncTask<Void, Void, Boolean> {
+
+		private final String username;
+		private final String token;
+		private final String type;
+		public String message;
+		
+		RetrievePeople(String username, String token, String type) {
+			this.username = username;
+			this.token = token;
+			this.type = type;
+		}
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+//			Query the login script with their entered username/password
+	        List<NameValuePair> postParams = new ArrayList<NameValuePair>(3);
+	        postParams.add(new BasicNameValuePair("user", username));
+	        postParams.add(new BasicNameValuePair("token", token));
+	        postParams.add(new BasicNameValuePair("type", type));
+
+			JSONObject requestPersons = JSONfunctions.getJSONfromURL("http://proj-309-R12.cs.iastate.edu/functions/social/requestPersons.php", postParams);					
+			
+			//Try and check if it succeeded
+			try {
+				String success = requestPersons.getString("success");
+				
+				//Return true on success
+				if ( success.equals("1") ) {
+					int curGuildMember = 0;
+					
+					//Ignoring Message/Sucess - add the friends/enemies
+					for (int i = 0; i < requestPersons.length() - 2; i++ ) {
+						
+						if ( type.equals("friends") ) {
+							String person = requestPersons.getString("" + i);;
+							friends.add(person);
+						} else if (type.equals("enemies") ) {
+							String person = requestPersons.getString("" + i);
+							enemies.add(person);
+						} else if (type.equals("guild") ) {
+							if ( curGuildMember >= requestPersons.getInt("numMembers")) {
+								break;
+							}
+							String person = requestPersons.getString("name" + curGuildMember);
+							person += " - " + requestPersons.getString("rank" + curGuildMember);
+							guilds.add(person);
+							curGuildMember++;
+						}
+						
+					}
+					
+					
+					
+					message = "success";
+					return true;
+					
+				//Set error message and return false.
+				} else {
+					message = requestPersons.getString("message");
+					return false;
+				}
+			
+			//Off chance that some weird shit happens
+			} catch (JSONException e) {
+				//Something went wrong - typically JSON value doesn't exist (success).
+				message = "An error occured. Please try again later.";
+				return false;
+			}
+			
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			if ( success ) {
+				listPeople(type);
+			} 
+		}
+
+		@Override
+		protected void onCancelled() {
+			//
+		}
+	}
+	
+	/**
+	 * Go to the main menu and close the current activity
+	 */
+	public void goToMain(){
+		finish();
+    	Intent main = new Intent(this, MainActivity.class);
+    	startActivity(main);
+	}
+
+	/**
+	 * Represents an asynchronous login/registration task used to authenticate
+	 * the user.
+	 */
+	public class addPlayer extends AsyncTask<Void, Void, Boolean> {
+
+		private final String myUser;
+		private final String otherUser;
+		private final String token;
+		public String message;
+
+		addPlayer(String myUser, String otherUser, String token) {
+			this.myUser = myUser;
+			this.otherUser = otherUser;
+			this.token = token;
+		}
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+//			Query the login script with their entered username/password
+	        List<NameValuePair> postParams = new ArrayList<NameValuePair>(3);
+	        postParams.add(new BasicNameValuePair("thisUser", myUser));
+	        postParams.add(new BasicNameValuePair("otherUser", otherUser));
+	        postParams.add(new BasicNameValuePair("token", token));
+	        
+			JSONObject requestFriends = JSONfunctions.getJSONfromURL("http://proj-309-R12.cs.iastate.edu/functions/social/addPlayer.php", postParams);					
+			
+			//Try and check if it succeeded
+			try {
+				String success = requestFriends.getString("success");
+				
+				//Return true on success
+				if ( success.equals("1") ) {
+					friends.add(otherUser);
+					message = otherUser + " added successfully";
+					return true;
+					
+				//Set error message and return false.
+				} else {
+					message = requestFriends.getString("message");
+					return false;
+				}
+			
+			//Off chance that some weird shit happens
+			} catch (JSONException e) {
+				//Something went wrong - typically JSON value doesn't exist (success).
+				message = "An error occured. Please try again later.";
+				return false;
+			}
+			
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			if ( success ) {
+				listPeople("friends");
+				toast(message);
+			} else {
+				toast(message);
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			//Nothing
+		}
+	}
 }
