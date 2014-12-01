7303b16826168eed3f7054d99b01ed5a35014391
jakleckler <Jakleckl@iastate.edu>
2014-12-01 16:33:48 -0600

    tyring to get the server stuff to work

---
+++ ConquestServer/conquest/server/classes/UpdateStatsRequest.java
@@ -0,0 +1,12 @@
+package conquest.server.classes;
+
+public class UpdateStatsRequest {
+	public String username;
+	public String token;
+	public int maxHealth;
+	public int curHealth;
+	public int attack;
+	public int armor;
+	public int money;
+
+}
