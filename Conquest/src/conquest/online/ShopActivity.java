d39d3d999d440f469fdf537685b9c9ef0e986f56
Paul <pgerlich@iastate.edu>
2014-12-04 17:52:43 -0600

    Fixed shit

--- Conquest/src/conquest/online/ShopActivity.java
+++ Conquest/src/conquest/online/ShopActivity.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import conquest.online.client.MovementClient;
