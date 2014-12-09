a0fad799093f2d5c9cfb640ce5134b9ee9e3f7b8
jakleckler <Jakleckl@iastate.edu>
2014-12-08 21:02:55 -0600

    added stock item, going to add to shop

---
+++ Conquest/src/conquest/online/gameAssets/Items/Stock.java
@@ -0,0 +1,21 @@
+package conquest.online.gameAssets.Items;
+
+public class Stock extends AbstractItem {
+	
+	private double gpm;
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void setGpm(double gpm) {
+		this.gpm = gpm;
+	}
+	
+	public double getGpm() {
+		return gpm;
+	}
+
+}
