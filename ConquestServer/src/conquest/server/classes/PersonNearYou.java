5fc32dc1edd119c8adc38c10554151be1622f0d1
Paul <pgerlich@iastate.edu>
2014-12-09 01:27:48 -0600

    Some shiz

---
+++ ConquestServer/src/conquest/server/classes/PersonNearYou.java
@@ -0,0 +1,14 @@
+package conquest.server.classes;
+
+public class PersonNearYou {
+	public boolean nearPeople;
+	public String user;
+	public String myClass;
+	public String guild;
+	public int level;
+	public int curHealth;
+	public int maxHealth;
+	public double x;
+	public double y;
+	public boolean isAly;
+}
